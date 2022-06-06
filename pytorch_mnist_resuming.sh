6d2d6b80fab8f2f016faf8b91465ea8d6045a571
Trond Linjordet <trond.linjordet@uis.no>
2022-06-06 16:54:45 +0200

    Update sbatch script to cancel array if finished

--- pytorch_mnist_resuming.sh
+++ pytorch_mnist_resuming.sh
@@ -6,7 +6,7 @@
 #SBATCH --job-name=pytorch_mnist_resuming
 #SBATCH --output=mnist_test_resuming.out
 
-TARGET_EPOCHS = 50
+TARGET_EPOCHS=50
 
 # Activate environment
 uenv verbose cuda-11.4 cudnn-11.4-8.2.4
@@ -16,11 +16,17 @@ conda activate pytorch_env
 # Run the Python script that uses the GPU 
 # and which checks if the target number 
 # of epochs has been reached.
-echo $SLURM_JOBID >  mnist_test_resuming-subtask_${SLURM_ARRAY_TASK_ID}.txt
-echo $SLURM_ARRAY_TASK_ID >  mnist_test_resuming-subtask_${SLURM_ARRAY_TASK_ID}.txt
+echo $SLURM_JOBID
+echo $SLURM_ARRAY_JOB_ID
+
+echo $SLURM_ARRAY_JOB_ID > mnist_test_resuming-subtask_${SLURM_ARRAY_TASK_ID}.txt
+echo $SLURM_ARRAY_TASK_ID >>  mnist_test_resuming-subtask_${SLURM_ARRAY_TASK_ID}.txt
 python -u pytorch_mnist_resuming.py --epochs $TARGET_EPOCHS --save-model > mnist_test_resuming-subtask_${SLURM_ARRAY_TASK_ID}.txt
 
 # Stop job array after the first subtask where the final checkpoint exists.
-if [test -f "mnist_cnn_epoch{$TARGET_EPOCHS}.pt"]; then
-  scancel $SLURM_JOBID
-  fi
+echo "Checking for mnist_cnn_epoch${TARGET_EPOCHS}.pt"
+if [ -f  "mnist_cnn_epoch${TARGET_EPOCHS}.pt" ]; then
+  echo "Final checkpoint exists. Cancelling array job."
+  scancel --state=PENDING $SLURM_ARRAY_JOB_ID
+  sleep 40
+fi
